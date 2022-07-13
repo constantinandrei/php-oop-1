b116d69475d7569291b982f83fc9ac8d7f5ab491
constantinandrei <andrei.todirascu@gmail.com>
2022-07-13 17:35:33 +0200

    add more movies

--- index.php
+++ index.php
@@ -3,10 +3,16 @@ require_once ('object.php');
 
 $movie1 = new Movie(1, 'Pulp Fiction', 'Quentin Tarantino');
 $movie1->addCast('actor', 'Bruce Willis');
-$movie1->addCast('actor', 'UmaThurman');
+$movie1->addCast('actor', 'Uma Thurman');
 
+$movie2 = new Movie(2, 'Titanic', 'James Camerun');
+$movie2->addCast('actor', 'Leonardo di Caprio');
 
+$movie3 = new Movie(3, 'Star Wars', 'George Lucas');
+$movie3-> addCast('dictator', 'Dart Fener');
 
 var_dump($movie1);
+var_dump($movie2);
+var_dump($movie3);
 
 ?>
--- object.php
+++ object.php
@@ -1,36 +1,49 @@
-<?php
+<?php 
+    class Movie {
+        public $id;
+        public $name;
+        public $director;
+        public $cast = [];
+    
+        function __construct ($_id, $_name, $_director){
+            $this->setId($_id);
+            $this->setName($_name);
+            $this->setDirector($_director);
+        }
+    
+        public function setId ($_id){
+            $this->id = $_id;
+        }
 
-class Movie 
-{
-    public $id;
-    public $name;
-    public $director;
-    public $cast = [];
+        public function getId (){
+            return $this->id;
+        }
+    
+        public function setName ($_name){
+            $this->name = $_name;
+        }
 
-    function __construct ($_id, $_name, $_director){
-        $this->setId($_id);
-        $this->setName($_name);
-        $this->setDirector($_director);
+        public function getname (){
+            return $this->name;
+        }
+    
+        public function setDirector ($_director){
+            $this->director = $_director;
+        }
+
+        public function getDirector (){
+            return $this->director;
+        }
+    
+        public function addCast($role, $name){
+            $this->cast[] = [
+                "role" => $role,
+                "name" => $name
+            ];
+        }
+
+        public function getCast (){
+            return $this->cast;
+        }
     }
-
-    public function setId ($_id){
-        $this->id = $_id;
-    }
-
-    public function setName ($_name){
-        $this->name = $_name;
-    }
-
-    public function setDirector ($_director){
-        $this->director = $_director;
-    }
-
-    public function addCast($role, $name){
-        $this->cast[] = [
-            "role" => $role,
-            "name" => $name
-        ];
-    }
-}
-
 ?>
